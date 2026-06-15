ebd7c250eaa8b1faf37ed97f4a0861f09e9ab59b
Fereshteh_BahadoryKhalily <65620341+fereshteh-bahadory@users.noreply.github.com>
2026-06-15 13:08:43 +0330

    Update converter_pr.py
    Add a function to convert digits to persian format

--- src/converter_pr.py
+++ src/converter_pr.py
@@ -1,32 +1,46 @@
 import re
 
-Path = "Your Path here"
-
-#make an html file to write the contents
-output_file_path = fr"{Path}\content_list_pr.html"
+#make an HTML file to write the contents
+output_file_path = r"F:\learn programming\python and django\py doc latex\content_list.html"
 MAX_LENGTH=120
+
+#This function is added to convert digits to Persian digits
+def to_persian_digits(text):
+    persian = '۰۱۲۳۴۵۶۷۸۹'
+    result = ''
+    for ch in str(text):
+        if ch.isdigit():
+            result += persian[int(ch)]
+        else:
+            result += ch
+    return result
+
+
 #open .doc file 
 with open(output_file_path, 'w', encoding='utf-8') as out_file:
     #just to organise the HTML file
     out_file.write('<html><head><meta charset="utf-8"></head>')
     out_file.write('<body dir="rtl" style="font-family:\'B Nazanin\', \'Vazirmatn\', Tahoma; font-size:14pt; line-height:2.2;">\n')
     
-    with open(fr"{Path}\file.toc",'r', encoding='utf-8') as file:
+    with open(r"F:\learn programming\python and django\py doc latex\main.toc",'r', encoding='utf-8') as file:
         content=file.read()
     
     for line in content.split('\n'):
         #Split different parts of every line to read the title and page number of each line
         if "\\contentsline" in line:
             parts=line.split("}")
-            #Since starred sections have a different order, we should separate them to read the context properly
+            #Since stared sections have different order, we should separate them to read the context properly
             if "section*" in line or "chapter*" in line:
                 title=parts[1].replace("{","")
                 title=title.replace("\u200c",chr(8204))
                 page=parts[2].replace("{","")
+                page = to_persian_digits(page)
             else:
                 title=parts[2].replace("{","")
                 title=title.replace("\u200c",chr(8204))
                 page=parts[3].replace("{","")
+                page = to_persian_digits(page)
+                
     
             if "\\numberline" in line:
                 line_parts=line.split('}{')
@@ -34,8 +48,10 @@ with open(output_file_path, 'w', encoding='utf-8') as out_file:
                 brace_start=line_number.find('{')
                 brace_end=line_number.find('}',brace_start)
                 number_sec=line_number[brace_start+1:brace_end]
-                title=number_sec+" " +title
-            #Order and tidy formulas in the content list, if there is any
+                number_sec_fa = to_persian_digits(number_sec)
+                title=number_sec_fa+" " +title
+
+            #Order and tidy formulas in the content list, if there are any
             title = re.sub(r'(\$.*?\$)', r'<span dir="ltr" style="font-family:Tahoma;">\1</span>', title)
 
             #Content list indentation
@@ -44,6 +60,8 @@ with open(output_file_path, 'w', encoding='utf-8') as out_file:
                 indent=""
             elif "subsubsection" in line:
                 indent="      "
+            elif "subsection" in line:
+                indent="    "
             elif "section" in line:
                 indent="  "
             
@@ -51,4 +69,3 @@ with open(output_file_path, 'w', encoding='utf-8') as out_file:
             title_with_dots = title_with_indent.ljust(MAX_LENGTH, ".")
             table_f = f'<div style="font-family:monospace, \'B Nazanin\'; white-space: pre-wrap; margin:0; text-align: right;">{title_with_dots} {page}</div>\n'
             out_file.write(table_f)
-        out_file.write('</body></html>')
